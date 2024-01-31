0f236bd1506d60f4b01041c73991280fdca4aa53
caspted <teddiejohn.rajeev-22@cpu.edu.ph>
2024-01-31 22:12:25 +0800

    [Fix][Ted] fixed percolation threshold error

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,8 @@
-import readline from 'readline-sync'
+import readlineSync from 'readline-sync'
 import PercolationStats from './percolationStats'
 
-
-const readlineSync = require("readline-sync");
-
-const n = parseInt(readlineSync.question("Enter grid size (n): "));
-const T = parseInt(readlineSync.question("Enter number of trials (T): "));
+const n = parseInt(readlineSync.question('Enter grid size (n): '), 10);
+const T = parseInt(readlineSync.question('Enter number of trials (T): '), 10);
 
 const percolationStats = new PercolationStats(n, T);
 
@@ -14,36 +11,3 @@ console.log(`Standard Deviation: ${percolationStats.stddev()}`);
 console.log(
   `95% Confidence Interval: [${percolationStats.confidenceLow()}, ${percolationStats.confidenceHigh()}]`
 );
-
-
-for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-        var randomValue = Math.random()
-        
-        if (randomValue < 0.593) {
-          process.stdout.write("⬛️");
-        } else {
-          process.stdout.write("🟦");
-        }
-    }
-    process.stdout.write('\n');
-}
-
-// let uf = new QuickUnionUF(N)
-// let input = readline.question()
-
-// while (input !== "") {
-//   let p = parseInt(input.split(' ')[0])
-//   let q = parseInt(input.split(' ')[1])
-
-//   if(!uf.connected(p, q)) {
-//     uf.union(p, q)
-//     console.log(`${p} and ${q} are connected`)
-//   }
-
-//   console.log(uf.ids)
-
-//   input = readline.question()
-// }
-
-
